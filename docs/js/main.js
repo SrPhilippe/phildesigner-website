1f574602451ca8a30132b5cdc71fb51813cfd981
SrPhilippe <philippehenrique@outlook.com>
2018-07-07 09:38:51 -0300

    Menu responsive interactions with jquery

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,18 +1,33 @@
 $(document).ready(() => {
 
-    $('#menu ul > li').each((index, el) => {
+    $('#menu .mobile > ul li').each((index, el) => {
         $(el).has('ul').children('a').append('<i class="fas fa-caret-right"></i>')
         $(el).children('ul').addClass('disabled')
     })
 
+    if ($(window).width() > 768) {
+        $('#menu .mobile > ul > li').has('ul').hover((event) => {
+            let el = $(event.currentTarget).children('ul')
+            el.toggleClass('active')
+            el.toggleClass('disabled')
+        }, (event) => {
+            let el = $(event.currentTarget).children('ul')
+            el.toggleClass('active')
+            el.toggleClass('disabled')
+        })
+    } else {
+        $('#menu .mobile > ul > li').has('ul').click((event) => {
+            $(event.currentTarget).children('ul').toggleClass('active')
+        })
+    }
 
-    $('#menu ul > li').has('ul').hover((event) => {
-        let el = $(event.currentTarget).children('ul')
-        el.toggleClass('active')
-        el.toggleClass('disabled')
-    }, (event) => {
-        let el = $(event.currentTarget).children('ul')
-        el.toggleClass('active')
-        el.toggleClass('disabled')
+    $('.navigation').click((event) => {
+        $(event.currentTarget).next().is((index, el) => {
+            $(el).toggleClass('active')
+        })
+    })
+
+    $('#menu .mobile > ul li > ul').click((event) => {
+        $(event.currentTarget).toggleClass('active')
     })
 })
